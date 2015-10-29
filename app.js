999add6b13664ed6487addb7cc4d70fa9b5d88ca
jocelynalsdorf <jfrodsla@gmail.com>
2015-10-29 13:03:31 -0700

    install and set up ejs template engine

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //reuire modules
 var express = require('express');
 var app = express();
+var ejs = require('ejs');
 
 //variables
 
@@ -10,7 +11,39 @@ var port = 3000;
 
 app.use(express.static(__dirname + "/public"));
 
+//configure expres to use a views engine and establish the directotry they are stored in
+app.set("views", root + "/views");
+app.set("view engine", "ejs");
+
+//make dynamic routes
+
+app.get("/ejs_test", function ejs_testCallBack(req, res) {
+ // res.send("why Hello!");
+  res.render("test");
+});
+
+
+app.get("/someJSON", function someJSONCallback (req, res){
+  res.json({
+    "one": {
+      "so": "cool"
+    },
+    "two": "supercool",
+    "three": ["love", "json", "and", "javascript"]
+  })
+});
+
+app.get("/beginning", function beginningCallback (req, res){
+  res.send("<h1>This is just the start</h1><p><3</p>")
+})
+//app.post();
+
+
+
 //start server
 app.listen(port, function listenCallback() {
   console.log("express server on port" + port);
 });
+
+
+
